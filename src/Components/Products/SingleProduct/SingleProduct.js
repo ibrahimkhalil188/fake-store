7754043dd07bac1a3e07b2dfe85df24da3f65ac3
unknown <ibrahimkhalil.mine@gmail.com>
2022-03-20 08:20:21 +0600

    product

--- src/Components/Products/SingleProduct/SingleProduct.js
+++ src/Components/Products/SingleProduct/SingleProduct.js
@@ -9,12 +9,8 @@ const SingleProduct = (props) => {
                 <h2>{title.slice(1, 30) + " ..."}</h2>
                 <h3 style={{ color: "coral" }}>Price: {price}$</h3>
                 <div className="button">
-                    <div>
-                        <button className='btn btn-success'>Add to Cart</button>
-                    </div>
-                    <div>
-                        <button className='btn btn-info'>Details</button>
-                    </div>
+                    <button className='btn btn-success'>Add to Cart</button>
+                    <button className='btn btn-info'>Details</button>
                     <button className='btn btn-danger'>Delete</button>
                 </div>
             </div>
